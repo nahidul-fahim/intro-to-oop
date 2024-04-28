bdf66e432a9b6594b6d11d6fd484a21751bb91cc
nahidul-fahim <nahidulfahim.official@gmail.com>
2024-04-28 23:42:44 +0600

    exploring inheritance

--- firstOOP.js
+++ firstOOP.js
@@ -1,5 +1,8 @@
 /*
 ------ What is a class? -------
+class is a blueprint of actual object. Object is an instance of class. We can create object from class.
+
+In another sentence, we can say,
 We can think of a class like a blueprint of a house. A class is not a real world object but we can create objects from a class. It is like an template for an object.
 
 We can create classes using the class keyword which is reserved keyword in JavaScript. Classes can have their own properties and methods.
@@ -21,6 +24,10 @@ class Student {
         this.dateOfBirth = dateOfBirth;
         this.roll = roll
     }
+
+    sayIntro() {
+        return `Hey there! My name is ${this.name} and my roll number is ${this.roll}`
+    }
 }
 
 const Nahid = new Student("Nahidul Islam", "22 November 2012", 1);
@@ -28,3 +35,5 @@ console.log(Nahid.name) // Nahidul Islam
 
 const Fahim = new Student("Fahim Islam", "22 November 2014", 3);
 console.log(Fahim.dateOfBirth) // 22 November 2014
+
+console.log(Nahid.sayIntro()) // Hey there! My name is Nahidul Islam and my roll number is 1
